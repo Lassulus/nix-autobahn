39f4dd8c70a932e02d0623a8b230dfb4fc165990
DavHau <hsngrmpf+github@gmail.com>
2022-04-16 13:18:34 +0200

    shellcheck + updates
    - update flake inputs
    - use pkgs.writeShellApplication, and add runtime deps as inputs
    - fix shelcheck errors

--- flake.nix
+++ flake.nix
@@ -2,25 +2,46 @@
   description = "Shell script collection to download ELF binaries and use them right away!";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }: let
+      l = nixpkgs.lib // builtins;
+
+      scriptNames =
+        l.filter
+        (script: l.hasPrefix "nix-autobahn" script)
+        (l.attrNames (l.readDir ./.));
+  in
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         packageName = "nix-autobahn";
+
+        scripts =
+          l.map
+            (scriptName:
+              pkgs.writeShellApplication {
+                name = scriptName;
+                text = l.readFile ("${./.}/${scriptName}");
+
+                runtimeInputs = with pkgs; [
+                  findutils
+                  fzf
+                  nix-index
+                  nix-ld
+                ];
+              })
+            scriptNames;
       in {
-        packages.${packageName} = pkgs.stdenv.mkDerivation {
-          name = packageName;
-          src = ./.;
-          installPhase = ''
-            echo nix-autobahn*
-            mkdir -p $out/bin
-            mv nix-autobahn* $out/bin
-          '';
-        };
+
+        packages.${packageName} = pkgs.runCommand "nix-autobahn" {} ''
+          mkdir -p $out/bin
+          for script in ${l.toString scripts}; do
+            cp $script/bin/* $out/bin/
+          done
+        '';
 
         defaultPackage = self.packages.${system}.${packageName};
       });
